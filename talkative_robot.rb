47f8158ae54e9c74f041fadb5b38a3482dc52475
cmarkel5 <cmarkel5@gmail.com>
2014-09-23 12:42:00 -0400

    first commit

---
+++ talkative_robot.rb
@@ -0,0 +1,32 @@
+puts "Input user's name (first & last)"
+user_name = gets.chomp.capitalize
+
+puts "Input user's age"
+user_age = gets.chomp.to_i
+
+puts "Hi #{user_name} who is #{user_age} years old!"
+puts "Do you mind if I call you #{user_name[0]}?"
+
+if user_age < 0
+	puts "Since the unborn cannot run this program. Please put in your real age."
+elsif user_age < 75
+	puts "You will be 75 years old in #{75 - user_age} years."
+elsif user_age > 75
+	puts "You turned 75 years old #{user_age - 75} years ago."
+else
+	puts "You are 75 years old."
+end
+
+user_name = user_name.upcase!
+
+puts "Hey #{user_name.chars.first}, where are you going!?"
+puts 'Yo "Dude", what\'s up?'
+
+puts "What did you have for breakfast?"
+breakfast = gets.chomp.downcase
+puts "I love #{breakfast}"
+
+puts "What are your plans for this evening?"
+evening_plans = gets.chomp.capitalize
+puts "#{evening_plans}, huh? I've been meaning to do that."
+
