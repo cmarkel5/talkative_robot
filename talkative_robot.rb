f289347a63fa2031688c07017971126a146fc59a
cmarkel5 <cmarkel5@gmail.com>
2014-09-23 16:39:53 -0400

    Added troubleshooting practices

--- talkative_robot.rb
+++ talkative_robot.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 puts "Input user's name (first & last)"
 user_name = gets.chomp.capitalize
 
@@ -5,10 +7,10 @@ puts "Input user's age"
 user_age = gets.chomp.to_i
 
 puts "Hi #{user_name} who is #{user_age} years old!"
-puts "Do you mind if I call you #{user_name[0]}?"
+puts "Do you mind if I call you #{user_name.chars.first}?"
 
 if user_age < 0
-	puts "Since the unborn cannot run this program. Please put in your real age."
+	puts "Since the unborn cannot run this program. Please input your real age."
 elsif user_age < 75
 	puts "You will be 75 years old in #{75 - user_age} years."
 elsif user_age > 75
@@ -19,7 +21,7 @@ end
 
 user_name = user_name.upcase!
 
-puts "Hey #{user_name.chars.first}, where are you going!?"
+puts "Hey #{user_name.split(" ")[0]}, where are you going!?"
 puts 'Yo "Dude", what\'s up?'
 
 puts "What did you have for breakfast?"
